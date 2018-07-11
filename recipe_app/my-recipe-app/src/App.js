3065429406485b708ebbdd7b99c566b191362c03
Kerin Khan <kerink585@gmail.com>
2018-07-10 17:58:39 -0700

    recreated app

--- recipe_app/my-recipe-app/src/App.js
+++ recipe_app/my-recipe-app/src/App.js
@@ -1,133 +1,25 @@
 import React, { Component } from 'react';
 
 import './App.css';
-const RECIPES =[
-  {
-    name: "waffles",
-    ingredients: ["1 egg","milk","flour"],
-    instructions: ["add ingredients to bowl and mix","heat iron to 450","pour 1/2 cup into iron"],
-  },
-  {
-    name: "PanCake",
-    ingredients: ["1 egg","milk","flour"],
-    instructions: ["add ingredients to bowl and mix","heat iron to 450","pour 1/2 cup into iron"],
-  }
-]
-function Header(props){
-  return(
-    <div className="header">
-      <h1>{props.title}</h1>
-    </div>
-  );
-}
-Header.propTypes ={
-  title: React.PropTypes.string.isRequired,
-  name: React.PropTypes.string.isRequired,
-}
-
-function ListOfRecipes(props){
-  return(
-    <div className="ordered-list">
-      <ol>
-        <li onClick= {function(){props.onChange()}}>
-          {props.name}
-        </li>
-      </ol>
-    </div>
-  )
-}
-ListOfRecipes.propTypes ={
-  name: React.PropTypes.string.isRequired,
-  onChange: React.PropTypes.func.isRequired,
-}
-
-function Recipe(props){
-  return(
-    <div className="recipe">
-      <div className="recipe-name">
-        <h2>{props.name}</h2>
-      </div>
-
-    </div>
-  );
-}
-Recipe.propTypes ={
-  name: React.PropTypes.string.isRequired,
-  ingredients: React.PropTypes.string.isRequired,
-  instructions:  React.PropTypes.string.isRequired,
-}
 
 class App extends Component {
-  propTypes:{
-    title: React.PropTypes.string.isRequired,
-    initialRecipes: PropTypes.array,
-  }
-
-  getDefaultProps: function(){
-    return{
-      title: "Recipe Journal",
-    }
-  }
-
-  getInitialState: function(){
-    return{
-      recipes: this.props.initialRecipes,
-    }
-  }
-  getListOfIngredients: function(){
-    let list =[];
-    for(let i=0;i<this.state.recipes.length;i++){
-      let children =[];
-      for(let j=0; j<this.state.recipes[i].ingredients.length; j++ ){
-        children.push(<li>{this.state.recipes[i].ingredients[j]}</li>)
-      }
-      for(let j=0; j<this.state.recipes[i].instructions.length; j++ ){
-        children.push(<li>{this.state.recipes[i].instructions[j]}</li>)
-      }
-      list.push(<ul>{children}</ul>)
-    }
-    return list;
-  }
-  getListOfInstructions: function(){
-    // need to get index of recipe click
-    let list =[];
-    for(let i=0;i<this.state.recipes.length;i++){
-      let children =[];
-      for(let j=0; j<this.state.recipes[i].instructions.length; j++ ){
-        children.push(<li>{this.state.recipes[i].instructions[j]}</li>)
-      }
-      list.push(<ul>{children}</ul>)
-    }
-    return list;
-  }
   render() {
     return (
       <div className="App">
-        <Header title={this.props.title}/>
-        {
-          this.state.recipes.map(function(recipe, index){
-            return(
-              <ListOfRecipes name={recipe.name} />
-            )
-          })
-        }
-          <div className="recipes">
-          {
-            this.state.recipes.map(function(recipe, index){
-              return(
-                <Recipe name={recipe.name} />
-              )
-            })
-          }
+        <div className="header">
+          <h1>Recipe Journal</h1>
+        </div>
+        <div className="intro-content">
+
+        </div>
+        <div className="main-content">
+          <div className="recipe">
+           <div className="recipe-name">
+           </div>
+           <div className="recipe-ingredients"></div>
+           <div className="recipe-instructions"></div>
           </div>
-          < div className="ingredients">
-            <p>Ingredients</p>
-            {this.getListOfIngredients()}
-          </div>
-          < div className="instructions">
-            <p>Instructions</p>
-            {this.getListOfInstructions()}
-          </div>
+        </div>
       </div>
     );
   }
--- recipe_app/my-recipe-app/src/index.js
+++ recipe_app/my-recipe-app/src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-const RECIPES =[
-  {
-    name: "waffles",
-    ingredients: ["1 egg","milk","flour"],
-    instructions: ["add ingredients to bowl and mix","heat iron to 450","pour 1/2 cup into iron"],
-  },
-  {
-    name: "PanCake",
-    ingredients: ["1 egg","milk","flour"],
-    instructions: ["add ingredients to bowl and mix","heat iron to 450","pour 1/2 cup into iron"],
-  }
-]
 
-ReactDOM.render(<App initialRecipes={RECIPES}/>, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
